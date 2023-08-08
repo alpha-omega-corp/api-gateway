1674850b936880b1db4edd29215d599e97c7944e
nanstis <bleyo@alphomega.org>
2023-08-08 09:56:48 +0200

    fix: middleware import

--- types/middleware.go
+++ types/middleware.go
@@ -2,7 +2,6 @@ package types
 
 import (
 	"errors"
-	"fmt"
 	"github.com/alpha-omega-corp/api-gateway/pkg/auth"
 	"github.com/alpha-omega-corp/auth-svc/proto"
 	"github.com/uptrace/bunrouter"
@@ -23,9 +22,6 @@ func NewAuthMiddleware(svc *auth.ServiceClient) *AuthMiddleware {
 func (m *AuthMiddleware) Auth(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
 	return func(w http.ResponseWriter, req bunrouter.Request) error {
 		header := req.Header.Get("Authorization")
-
-		fmt.Print(header)
-
 		token := strings.Split(header, "Bearer ")[1]
 
 		res, err := m.svc.Client.Validate(req.Context(), &proto.ValidateRequest{
